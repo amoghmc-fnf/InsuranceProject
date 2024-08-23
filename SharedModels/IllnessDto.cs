def7d1335c7ae5f2c39f3b70171544e75c77ba09
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-08-23 10:52:33 +0530

    remove old monolith code

--- SharedModels/IllnessDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InsuranceApi.DTOs
-{
-	public class IllnessDto
-	{
-		public int IllnessId { get; set; }
-        public string Name { get; set; } = null!;
-        public string Description { get; set; } = null!;
-    }
-}
--- SharedModels/InsuredIllnessDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InsuranceApi.DTOs
-{
-    public class InsuredIllnessDto
-    {
-        public int InsuredIllnessId { get; set; }
-        public int InsuredId { get; set; }
-        public int IllnessId { get; set; }
-    }
-}
