70fdd58785685e239af8a9f2c5c698deeb5d9dd8
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-08-28 09:52:26 +0530

    make dll for jwt models

--- JwtApi/Controllers/AuthController.cs
+++ JwtApi/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using JwtApi.Models;
+﻿using JwtModels;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
--- JwtModels/UserLogin.cs
+++ JwtModels/UserLogin.cs
@@ -1,4 +1,4 @@
-﻿namespace JwtApi.Models
+﻿namespace JwtModels
 {
     public class UserLogin
     {
